e6766f660e43294f3cb15494370911865434071e
RodionLe <oormorking@mail.ru>
2017-07-07 22:08:54 +0300

    first commit

---
+++ 1.60(a).py
@@ -0,0 +1,9 @@
+a=(input("ведите число a "))
+b=(input("ведите число b "))
+c=(input("ведите число c "))
+x=b
+z=a
+a=x
+b=c
+c=z
+print(a,b,c)
---
+++ 1.61(e).py
@@ -0,0 +1,7 @@
+a=int(input("введите число "))
+a *=a
+x = a
+a *=a
+a *=a
+z = a * x
+print(z)
