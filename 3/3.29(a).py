bdcee25ad380d0ad827c4e9d77b6925dbdb3d458
RodionLe <oormorking@mail.ru>
2017-07-30 13:08:44 +0300

    папки homework

--- 3/3.29(a).py
+++ 3/3.29(a).py
@@ -1,6 +1,6 @@
 x = int(input("введите число "))
 y = int(input("введите число "))
-if x%2!=0 and y%2!=0:
+if x % 2 != 0 and y % 2 != 0:
     print(x, y)
 else:
     print("(((((")
--- 3/3.33(з).py
+++ 3/3.33(з).py
@@ -1,10 +1,8 @@
 x = int(input("Введите x "))
 y = int(input("Введите y "))
-if x <= 1 and y >= 1.5:
+if y >= 1.5:
     print("Истина")
 elif x >= 1 and x <= 2.5 and y >= -2:
-    print("Истина")
-elif x >= 2.5 and y >= 1.5:
     print("Истина")
 else:
     print("Лож")
