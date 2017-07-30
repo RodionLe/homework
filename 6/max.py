e61c557d3fd0ba5e21e3e9050063fc82e2673d61
RodionLe <oormorking@mail.ru>
2017-07-30 14:44:16 +0300

    len

---
+++ 6/max.py
@@ -0,0 +1,8 @@
+x = int(input("Введите число "))
+max = x
+
+while x != 0:
+    x = int(input("введите число "))
+    if x > max:
+        max = x
+print(max)
---
+++ 6/polindrom.py
@@ -0,0 +1,4 @@
+str = input()
+print(len(str))
+print(str[0])
+print(str[len(str) - 1])
