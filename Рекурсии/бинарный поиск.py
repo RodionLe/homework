05d46d4f4595b006ade62b405106423e9d6949f0
oleg-tgn <stelmah.taganrog@gmail.com>
2018-03-17 17:25:52 +0300

    bin

--- Рекурсии/бинарный поиск.py
+++ Рекурсии/бинарный поиск.py
@@ -1,21 +1,25 @@
 number = [5, 12, 24, 34, 46, 70,  90 , 110, 140, 180, 250, 333, 438, 549, 674, 890, 1000]
 choice = int(input("Введите число: "))
 length = len(number)
-y = choice
-x = length // 2
-p = x
-def search(y, x, p):
-    if y not in number:
-        return print("Элемента в массиве нет")
-    else:
-        if y > number[p]:
-            p -= x
-            x //= 2
-            return search(y,x,p)
-        elif y < number[p]:
-            p += x
-            x //= 2
-            return search(y,x,p)
-        else:
-            return print("номер числа в массиве: ",p)
-search(y, x, p)
+
+def search(choice, left, right):
+    x = (right - left) // 2
+    p = right - x
+    
+    if x == 0:
+        x = 1
+     
+    if choice == number[p]:
+        return p
+    
+    if right == left:        
+        return -1       
+    
+    if choice > number[p]:        
+        return search(choice, left + x, right)
+    else: 
+        return search(choice, left, right - x)
+        
+result = search(choice, 0, length - 1)
+
+print(result)
